6c8b808e615a97f3da8bff9d72b46957955bcd55
gylman <gylmanbm@gmail.com>
2024-02-01 13:07:21 +0900

    toggle state on mouseenter/leave and log for testing

--- src/contexts/PorticoCtx.jsx
+++ src/contexts/PorticoCtx.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 export const PorticoCtx = createContext({
   _2ndScoll: false,
@@ -10,9 +10,13 @@ export const usePortico = () => useContext(PorticoCtx);
 export const ContextProvider = ({ children }) => {
   const [_2ndScoll, set_2ndScroll] = useState(false);
 
-  const handle2ndScroll = (handler) => {
-    set_2ndScroll(handler);
+  const handle2ndScroll = () => {
+    set_2ndScroll((previous) => !previous);
   };
+
+  useEffect(() => {
+    console.log(_2ndScoll ? 'in' : 'out');
+  }, [_2ndScoll]);
 
   return <PorticoCtx.Provider value={{ _2ndScoll, handle2ndScroll }}>{children}</PorticoCtx.Provider>;
 };

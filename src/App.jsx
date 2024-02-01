35371fb96ca729ccd64067be1f18b8ff82215bf3
gylman <gylmanbm@gmail.com>
2024-02-01 13:06:54 +0900

    prepare new influxDB query

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import styled, { keyframes } from 'styled-components';
 import Navbar from './components/Navbar';
-import first from './assets/images/first.svg';
-import second from './assets/images/second.svg';
-import third from './assets/images/third.svg';
-import fourth from './assets/images/fourth.svg';
 import to from './assets/images/to.svg';
-import key from './assets/images/key.svg';
-import asterisk from './assets/images/asterisk.svg';
 import cuid from 'cuid';
-import Liveness from './components/animation/Liveness';
-import RippleAnim from './components/Dot';
 import Dot from './components/Dot';
-import First from './components/First';
-import Table from './components/TableSection';
-import TableSection from './components/TableSection';
-// import axios from 'axios';
-// import Test from './components/Test';
+import axios from 'axios';
+import {
+  Root,
+  Content,
+  Menu,
+  MenuItem,
+  MenuText,
+  MainWrapper,
+  Main,
+  TagContainer,
+  Tag,
+  Head,
+  ImgWrapper,
+  Body,
+  LinksButtons,
+  BaseBtn,
+  TransButton,
+  Txt,
+  IconWrapper,
+} from './AppStyles';
+import { menuItems, views } from './components/imports';
 
 // Links to redirect
 
@@ -24,231 +31,12 @@ import TableSection from './components/TableSection';
 // twitter => https://twitter.com/radius_xyz
 // Docs button => https://github.com/radiusxyz
 
-const menuItems = [
-  { id: 0, text: 'Decentralized Shared Sequencing Layer' },
-  { id: 1, text: 'MEV / Censorship Resistance' },
-  { id: 2, text: 'Sequencer Liveness' },
-  { id: 3, text: 'Multi-Rollup Sequencing' },
-];
-
-const views = [
-  {
-    id: 0,
-    header: 'Decentralized \n Shared Sequencing Layer',
-    body: <First />,
-    linksAndButtons: [{ type: 0, text: 'Docs', icon: to, link: 'https://mindful-subtasks-250940.framer.app/' }],
-  },
-  {
-    id: 1,
-    tag: {
-      icon: key,
-      text: 'MEV and Censorship Resistance',
-    },
-    header: 'Leader-based Shared Sequencer Network',
-    body: <img src={second} />,
-    linksAndButtons: [
-      { type: 1, text: 'Learn more', icon: to, link: 'https://mindful-subtasks-250940.framer.app/' },
-      { type: 0, text: 'See Demo', link: 'https://mindful-subtasks-250940.framer.app/' },
-    ],
-  },
-  {
-    id: 2,
-    tag: {
-      icon: asterisk,
-      text: 'Sequencer Liveness',
-    },
-    header: 'Encrypted Mempool, \n Zero Knowledge Proof',
-    body: <Liveness />,
-    linksAndButtons: [
-      { type: 1, text: 'Learn more', icon: to, link: 'https://mindful-subtasks-250940.framer.app/' },
-      { type: 0, text: 'View Real-Time Logs', link: 'https://mindful-subtasks-250940.framer.app/' },
-    ],
-  },
-  {
-    id: 3,
-    header: 'Multi-Rollup Sequencing',
-    body: <TableSection />,
-    linksAndButtons: [{ type: 0, text: 'Learn more', icon: to, link: 'https://mindful-subtasks-250940.framer.app/' }],
-  },
-];
-
 const token = import.meta.env.VITE_INFLUXDB_TOKEN;
 const url = import.meta.env.VITE_INFLUXDB_URL;
 const org = 'RadiusLab';
-const bucket = 'logylman';
+const bucket = 'sequencer';
 
-const query = `
-from(bucket: "${bucket}")
-  |> range(start: -1d)
-  |> filter(fn: (r) => r._measurement == "logEntry")
-  |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
-`;
-
-const Root = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  background: #090a0f;
-  flex-direction: column;
-`;
-
-const Content = styled.div`
-  display: flex;
-  height: 100%;
-  padding: 0px 48px;
-  gap: 42px;
-`;
-
-const Menu = styled.div`
-  display: flex;
-  margin-right: 42px;
-  justify-content: flex-end;
-  height: 100%;
-  max-width: 320px;
-  width: 100%;
-  gap: 16px;
-  padding-bottom: 86px;
-  flex-direction: column;
-`;
-
-const MenuItem = styled.div`
-  position: relative;
-`;
-
-const MenuText = styled.p`
-  color: ${({ $active }) => ($active && '#fff') || 'rgba(255, 255, 255, 0.16)'};
-  font-family: Manrope;
-  font-size: 16px;
-  font-weight: 500;
-  width: 100%;
-  cursor: pointer;
-  margin-left: 16px;
-  &:hover {
-    color: ${({ $active }) => ($active && '#fff') || 'rgba(255, 255, 255, 0.46)'};
-  }
-  @media (max-width: 1919px) {
-    font-size: 14px;
-  }
-`;
-
-const MainWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-`;
-
-const Main = styled.div`
-  opacity: 0;
-  transform: translateY(40px);
-  transition: opacity 1s ease, transform 1s ease;
-
-  &.active {
-    opacity: 1;
-    transform: translateY(0);
-  }
-
-  display: flex;
-  justify-content: center;
-  align-items: stretch;
-  width: 100%;
-  max-width: 1100px;
-  flex-direction: column;
-  @media (max-width: 1919px) {
-    max-width: none;
-  }
-`;
-
-const TagContainer = styled.div`
-  display: flex;
-  width: 100%;
-`;
-
-const Tag = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 12px 18px;
-  gap: 6px;
-  border-radius: 9999px;
-  border: 1px solid #5c5b5e;
-  background: #141414;
-  margin-bottom: 36px;
-
-  color: #fff;
-  font-family: Manrope;
-  font-size: 16px;
-  font-weight: 500;
-  line-height: normal;
-  letter-spacing: 0.1px;
-`;
-
-const Head = styled.p`
-  color: #fff;
-  leading-trim: both;
-  text-edge: cap;
-  font-family: 'area-variable';
-  font-size: 52px;
-  font-weight: 400;
-  line-height: 72px; /* 138.462% */
-  margin-bottom: 64px;
-  white-space: pre-line;
-  @media (max-width: 1919px) {
-    font-size: 40px;
-    margin-bottom: 44px;
-    white-space: normal;
-  }
-`;
-
-const ImgWrapper = styled.div`
-  border-radius: 24px;
-  border: 1px solid rgba(92, 91, 94, 0.4);
-`;
-
-const Body = styled.div``;
-
-const LinksButtons = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin-top: 24px;
-  width: 100%;
-`;
-
-const BaseBtn = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  border-radius: 9999px;
-  border: 1px solid rgba(255, 255, 255, 0.4);
-  background: rgba(255, 255, 255, 0.1);
-  padding: 16px 30px;
-  cursor: pointer;
-`;
-
-const TransButton = styled(BaseBtn)`
-  border: none;
-  background: transparent;
-`;
-
-const Txt = styled.span`
-  display: flex;
-  justify-content: center;
-  align-self: flex-end;
-  color: #fff;
-  vertical-align: middle;
-  text-align: center;
-  font-family: 'area-variable';
-  font-size: 14px;
-  font-weight: 700;
-  line-height: 20px; /* 142.857% */
-`;
-
-const IconWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+const query = `from(bucket: "${bucket}") |> range(start: -7d) |> filter(fn: (r) => r["_measurement"] == "log") |> filter(fn: (r) => r["_time"] <= 2024-01-30T02:42:06.51511704Z)`;
 
 function App() {
   const handleMenuItem = (e) => {
@@ -325,6 +113,40 @@ function App() {
       window.removeEventListener('wheel', handleScroll);
     };
   }, [views]);
+
+  const [constructed, setConstructed] = useState([]);
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+
+  async function queryData() {
+    const headers = {
+      Authorization: `Token ${token}`,
+      'Content-Type': 'application/json',
+    };
+
+    const data = {
+      query: query,
+      type: 'flux',
+    };
+
+    try {
+      const response = await axios.post(`${url}/api/v2/query?org=${org}`, data, { headers });
+      console.log(response.data);
+      const newData = response.data
+        .split('\n')
+        .map((line) => line.split(','))
+        .filter((arr, index) => arr.length > 1 && index !== 0)
+        .map((arr) => ({ data: arr[7], fid: arr[8], from: arr[9], tid: arr[10], to: arr[11].slice(0, -1) }));
+
+      setConstructed(newData);
+      setIsDataLoaded(true);
+    } catch (error) {
+      console.error('QUERY ERROR', error);
+    }
+  }
+
+  useEffect(() => {
+    queryData();
+  }, []);
 
   return (
     <Root>

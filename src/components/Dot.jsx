d3c43cb3049aa402b7773f4eafb736cd9609ff40
gylman <gylmanbm@gmail.com>
2024-02-06 10:50:24 +0900

    implement dot in the center of menu item dot animation

--- src/components/Dot.jsx
+++ src/components/Dot.jsx
@@ -48,34 +48,3 @@ function Dot({ active }) {
 }
 
 export default Dot;
-
-// import { addPropertyControls, ControlType } from "framer"
-// import { motion } from "framer-motion"
-
-// function RippleAnim(props) {
-//     const css = ` .center {
-//         transform: translate(9px, 9px); /* Adjust these values to position your center element */
-//     }.nc-loop-ripple-16-icon-f{--animation-duration:1.8s;}.nc-loop-ripple-16-icon-f *{transform-origin:50% 50%;animation:nc-loop-ripple-anim var(--animation-duration) infinite cubic-bezier(.215,.61,.355,1)}.nc-loop-ripple-16-icon-f :nth-child(2){animation-delay:calc(var(--animation-duration)/-2)}@keyframes nc-loop-ripple-anim{0%{opacity:1;transform:scale(.2)}100%{opacity:0;transform:scale(1)}}`
-
-//     return (
-//         <svg
-//             height="24"
-//             width="24"
-//             viewBox="0 0 24 24"
-//             xmlns="http://www.w3.org/2000/svg"
-//         >
-//             <g fill="#090a0f">
-//                 <g className="nc-loop-ripple-16-icon-f">
-//                     <circle cx="12" cy="12" fill="#FFB800" r="12" />
-//                     <circle cx="12" cy="12" fill="#FFB800" r="12" />
-//                 </g>
-//                 <g className="center">
-//                     <circle cx="3" cy="3" fill="#FFA800" r="3" />
-//                 </g>
-//                 <style>{css}</style>
-//             </g>
-//         </svg>
-//     )
-// }
-
-// export default RippleAnim

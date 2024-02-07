1a2dc98d4c56692b6f044648817954945bf9f616
gylman <gylmanbm@gmail.com>
2024-02-07 16:38:00 +0900

    logserver with logs and roles being updated every 4 secs

---
+++ data.js
@@ -0,0 +1,29 @@
+export const iRoles = {
+  '0x1': 'l',
+  '0x2': 'f0',
+  '0x3': 'f1',
+  '0x4': 'f2',
+  '0x5': 'f3',
+  A: 'r0',
+  B: 'r1',
+  u: 'u',
+  timestamp: Date.now(),
+};
+
+export const iLogs = [
+  { from: 'u', to: '0x3', data: 'tx' },
+  { from: '0x3', to: '0x1', data: 'tx' },
+  { from: '0x1', to: '0x4', data: 'oc' },
+  { from: '0x1', to: '0x2', data: 'oc' },
+  { from: '0x1', to: '0x3', data: 'oc' },
+  { from: '0x3', to: 'u', data: 'oc' },
+  { from: 'u', to: '0x2', data: 'tx' },
+  { from: '0x2', to: '0x1', data: 'tx' },
+  { from: '0x1', to: '0x2', data: 'oc' },
+  { from: '0x2', to: 'u', data: 'oc' },
+  { from: '0x1', to: 'A', data: 'block' },
+].map((log, index) => {
+  return { ...log, timestamp: iRoles.timestamp + index };
+});
+
+console.log(iLogs);
--- index.js
+++ index.js
@@ -1,109 +1,41 @@
 import express from 'express';
-import axios from 'axios';
 import cors from 'cors';
+import { iLogs, iRoles } from './data.js';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 const port = 4000;
+const time = 4000;
 
-const token = 'nz-3Z97EJQYompt-o2hF5EK1YU8xiZpqoaeQGeI3uAuE2pOaboBliu1AA1cEaRrZtzqipRoIbuzOr8CSksTsmA==';
-const url = 'http://3.35.234.132';
-const org = 'RadiusLab';
-// const client = new InfluxDB({ url, token });
+const logs = [...iLogs];
+const roles = { ...iRoles };
 
-const corsOptions = {
-  origin: ['http://192.168.12.255', 'http://192.168.12.4'],
-  optionsSuccessStatus: 200, // Replace with the domain of your client app
-  allowedHeaders: ['Authorization', 'Content-Type'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-};
-
-async function queryLogs(token, url, org, timestamp) {
-  const query = `from(bucket: "sequencer") |> range(start: -7d) |> filter(fn: (r) => r["_measurement"] == "log") |> filter(fn: (r) => r["at"] > "${
-    timestamp ? timestamp : '0'
-  }") |> sort(columns: ["at"])`;
-  const headers = {
-    Authorization: `Token ${token}`,
-    'Content-Type': 'application/json',
-  };
-
-  const data = {
-    query: query,
-    type: 'flux',
-  };
-
-  try {
-    const response = await axios.post(`${url}/api/v2/query?org=${org}`, data, { headers });
-    const lines = response.data.split('\n');
-    const result = lines.map((line) => {
-      const fields = line.split(',');
-
-      return {
-        data: fields[6]?.replace(/"/g, ''),
-        from: fields[10]?.replace(/"/g, ''),
-        to: fields[11]?.replace(/"/g, '').replace('\r', ''),
-        timestamp: fields[9],
-      };
+setInterval(() => {
+  if (logs.length) {
+    const newLog = logs.slice(-3).map((log, index) => {
+      const newLogClone = { ...log };
+      newLogClone.timestamp = logs[logs.length - 1].timestamp + index + 1;
+      return newLogClone;
     });
-    result.shift(); // Remove the first line which might not be relevant
-    result.pop(); // Remove the last line which might be empty
-    result.pop(); // Additional pop in case there's another trailing line
-    return result;
-  } catch (error) {
-    console.error('QUERY LOGS ERROR', error);
+    roles.timestamp = newLog[newLog.length - 1].timestamp;
+    logs.push(...newLog);
+    console.log(roles.timestamp);
+  } else {
+    console.log('No logs to duplicate.');
   }
-}
-
-async function queryRoles(token, url, org) {
-  const query = `from(bucket: "sequencer_table") |> range(start: -7d) |> filter(fn: (r) => r["_measurement"] == "log") |> group(columns: ["id"]) |> last()`;
-  const headers = {
-    Authorization: `Token ${token}`,
-    'Content-Type': 'application/json',
-  };
-
-  const data = {
-    query: query,
-    type: 'flux',
-  };
-
-  try {
-    const response = await axios.post(`${url}/api/v2/query?org=${org}`, data, { headers });
-    const lines = response.data.split('\n');
-    const result = {};
-    lines.forEach((line, index, arr) => {
-      if (index !== 0 && index < arr.length - 2) {
-        const fields = line.split(',');
-        result[fields[9]?.replace(/"/g, '').replace('\r', '')] = fields[10]?.replace(/"/g, '').replace('\r', '');
-      }
-    });
-    return result;
-  } catch (error) {
-    console.error('QUERY ROLES ERROR', error);
-  }
-}
-
-// Call queryLogs directly
-//  '1706846834327471337'
-// const logs = await queryLogs(token, url, org);
-// const roles = await queryRoles(token, url, org);
-// console.log(logs);
-// console.log(roles);
-
-// app.get('/logs', cors(corsOptions), (req, res) => {
-//   // res.json(roles);
-//   res.json(logs);
-// });
-
-app.post('/logs', (req, res) => {
-  // res.json(roles);
-  console.log('hit');
-
-  res.json(['a', 'd']);
+}, time);
+app.get('/roles', (_, res) => {
+  res.json(roles);
 });
 
-app.get('/roles', (req, res) => {
-  // res.json(roles);
-  res.json({ bye: 'world' });
+app.post('/logs', (req, res) => {
+  const { timestamp } = req.body;
+  const response = logs.filter((log) => log.timestamp > timestamp);
+
+  setTimeout(() => {
+    res.json(response);
+  }, 5000);
 });
 
 app.listen(port, () => {

9bf3d586ed7820db41b5d0c5f17aa9d471a4ae4d
gylman <gylmanbm@gmail.com>
2024-02-13 15:55:37 +0900

    multi-rollup blocks endpoints

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const port = 4000;
-const time = 4000;
+const time = 15000;
 
 const logs = [...iLogs];
 const roles = { ...iRoles };
 
-setInterval(() => {
-  if (logs.length) {
-    const newLog = logs.slice(-2).map((log, index) => {
-      const newLogClone = { ...log };
-      newLogClone.timestamp = logs[logs.length - 1].timestamp + index + 1;
-      return newLogClone;
-    });
-    roles.timestamp = newLog[newLog.length - 1].timestamp;
-    logs.push(...newLog);
-  } else {
-    console.log('No logs to duplicate.');
-  }
-}, time);
+// setInterval(() => {
+//   if (logs.length) {
+//     const newLogs = logs.map((log, index) => {
+//       const newLogClone = { ...log };
+//       newLogClone.timestamp = logs[logs.length - 1].timestamp + index + 1;
+//       return newLogClone;
+//     });
+//     roles.timestamp = newLogs[newLogs.length - 1].timestamp;
+//     logs.push(...newLogs);
+//   } else {
+//     console.log('No logs to duplicate.');
+//   }
+// }, time);
 
-app.get('/roles', (_, res) => {
+app.post('/roles', (_, res) => {
   setTimeout(() => {
     res.json(roles);
   }, 2000);
@@ -42,13 +42,57 @@ app.post('/logs', (req, res) => {
     ++index;
   } else {
     setTimeout(() => {
-      // console.log(response);
       // roles.timestamp = response[response.length - 1]?.timestamp;
       index = 0;
       res.json(response);
     }, 5000);
   }
 });
+
+app.post('/mock', (req, res) => {
+  const { timestamp } = req.body;
+  console.log(timestamp);
+  const response = iLogs.filter((log) => log.timestamp > timestamp);
+  console.log(response);
+  res.json(response);
+  // if (index < 3) {
+  //   res.json([]);
+  //   ++index;
+  // } else {
+  //   setTimeout(() => {
+  //     // roles.timestamp = response[response.length - 1]?.timestamp;
+  //     index = 0;
+  //     res.json(response);
+  //   }, 5000);
+  // }
+});
+
+const A = [];
+const B = [];
+
+setInterval(() => {
+  A.push({ timestamp: Date.now() });
+}, 3000);
+
+setInterval(() => {
+  B.push({ timestamp: Date.now() });
+}, 10000);
+
+app.post('/getHeight', (req, res) => {
+  const { id } = req.body;
+  const response = id === 0 ? A.length : B.length;
+  console.log('/getHeight', response);
+  res.json(response);
+});
+
+app.post('/getTimestamp', (req, res) => {
+  const { id, height } = req.body;
+  const response = id === 0 ? A[height - 1] : B[height - 1];
+  console.log('getTimestamp', response);
+  res.json(response);
+});
+
+app.post('/rollup');
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);

ce91a787fbbbf8b9beb4927247655d9f2426b8da
gylman <gylmanbm@gmail.com>
2024-02-15 11:38:19 +0900

    remove redundancy

--- src/components/Fourth.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Fourth = () => {
-  return <div>Fourth</div>;
-};
-
-export default Fourth;
--- src/components/Third.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Third = () => {
-  return <div>Third</div>;
-};
-
-export default Third;
